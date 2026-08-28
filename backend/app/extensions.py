b73a1cf065cfeffabc42d8cd198e49bbe0ca91d9
Devansh Malhotra <malhotradevansh8@gmail.com>
2026-08-28 16:20:42 +0530

    Update extensions.py to use REDIS_URL

--- backend/app/extensions.py
+++ backend/app/extensions.py
@@ -22,9 +22,14 @@ def init_extensions(app):
     user_datastore = SQLAlchemyUserDatastore(db, User, Role)
     security.init_app(app,user_datastore)
 
-    redis_client = Redis(host=app.config["REDIS_HOST"],
-                         port = app.config["REDIS_PORT"],
-                         db = app.config["REDIS_DB"],
-                         decode_responses=True)
+    import os
+    redis_url = os.environ.get("REDIS_URL")
+    if redis_url:
+        redis_client = Redis.from_url(redis_url, decode_responses=True)
+    else:
+        redis_client = Redis(host=app.config["REDIS_HOST"],
+                             port=app.config["REDIS_PORT"],
+                             db=app.config["REDIS_DB"],
+                             decode_responses=True)
     
     celery.conf.update( broker_url = app.config["CELERY_BROKER_URL"], result_backend = app.config["CELERY_RESULT_BACKEND"], timezone = app.config["TIMEZONE"], accept_content = app.config["CELERY_ACCEPT_CONTENT"], task_serializer = app.config["CELERY_TASK_SERIALIZER"],)
